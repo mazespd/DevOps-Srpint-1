3da228ec79993f0bc4de2e0419433076051198b0
Pavel <pasha_smolin@inbox.ru>
2023-09-30 11:30:30 +0700

    Srpint-1

---
+++ modules/instance/main.tf
@@ -0,0 +1,64 @@
+data "yandex_compute_image" "my_image" {
+  family = var.instance_family_image
+}
+
+
+# Provider
+provider "yandex" {
+  token     = var.yandex_cloud_token
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  zone = var.zone[0]
+}
+
+# Service accounts
+resource "yandex_iam_service_account" "sf-admin" {
+  name = "pavel-sf-diplom"
+}
+
+  folder_id = var.yandex_folder_id
+  role = "admin"
+  member = "serviceAccount:${yandex_iam_service_account.sf-admin.id}"
+  depends_on = [
+    yandex_iam_service_account.sf-admin,
+  ]
+}
+
+# Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "static-access-key" {
+  service_account_id = yandex_iam_service_account.sf-admin.id
+  depends_on = [
+    yandex_iam_service_account.sf-admin,
+  ]
+}
+
+# Compute instance for service
+# Создаём ВМ - srv сервисную ноду, с которой будет просиходить развёртывание кластера k8s, мониторинг, логирование и процессы CI/CD
+resource "yandex_compute_instance" "srv" { 
+  name     = "srv"
+  hostname = "srv"
+
+  resources {
+    cores  = 4
+    memory = 12
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.my_image.id
+      size     = 30
+      type     = "network-ssd"
+    }
+  }
+
+  network_interface {
+    subnet_id = var.vpc_subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_credentials.user}:${file(var.ssh_credentials.pub_key)}"
+    serial-port-enable=1
+  }
+
+}
